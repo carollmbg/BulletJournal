8eb5ced0f1493778203e69104bcc52955ee4566b
carol <cmramos.apoioprodesp.sp.gov.br>
2022-05-25 17:04:33 -0300

    código inicializando corretamente

--- src/main/java/com/bulletjournal/bulletjournal/controller/PostagemController.java
+++ src/main/java/com/bulletjournal/bulletjournal/controller/PostagemController.java
@@ -5,8 +5,6 @@ import java.util.List;
 import com.bulletjournal.bulletjournal.model.PostagemModel;
 import com.bulletjournal.bulletjournal.repository.PostagemRepository;
 
-import org.apache.catalina.connector.Response;
-import org.hibernate.annotations.GeneratorType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
