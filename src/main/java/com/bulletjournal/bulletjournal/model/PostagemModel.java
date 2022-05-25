f307448a7e3d5409319dcc96dc2c8b487405cab8
carol <cmramos.apoioprodesp.sp.gov.br>
2022-05-25 17:59:36 -0300

    add relação many to one com a tabela tema

--- src/main/java/com/bulletjournal/bulletjournal/model/PostagemModel.java
+++ src/main/java/com/bulletjournal/bulletjournal/model/PostagemModel.java
@@ -5,11 +5,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 @Entity
@@ -57,6 +60,12 @@ public class PostagemModel {
     public void setDate(Date date) {
         this.date = date;
     }
+
+    //Aula 13/04
+
+    @ManyToOne
+    @JsonIgnoreProperties("post")
+    private Tema tema;
 
     
 }
