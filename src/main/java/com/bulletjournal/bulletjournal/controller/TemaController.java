0c644795bc3267cdc8eeccfc98f50c39e9c5204e
carol <cmramos.apoioprodesp.sp.gov.br>
2022-05-27 20:47:53 -0300

    controller finalizado

--- src/main/java/com/bulletjournal/bulletjournal/controller/TemaController.java
+++ src/main/java/com/bulletjournal/bulletjournal/controller/TemaController.java
@@ -14,10 +14,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 
@@ -62,6 +61,9 @@ public class TemaController {
         return ResponseEntity.ok(repository.save(tema));
     }
 
-    @DeleteMapping
-    public ResponseEntity                                         
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable long id)
+    {
+        repository.deleteById(id);
+    }                                        
 }
